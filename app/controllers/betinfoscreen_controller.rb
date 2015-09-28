e4198ac66917cee52311b94907e72a507dae7d50
dev <dev@Chris-Mallons-MacBook-Pro.local>
2015-09-28 21:41:17 +0100

    added rest of support files

---
+++ app/controllers/betinfoscreen_controller.rb
@@ -0,0 +1,7 @@
+class BetinfoscreenController < ApplicationController
+  def welcome
+  
+	@welcomeMsg = "Welcome to the betinfo menu. Please choose an option"
+
+  end
+end
---
+++ app/helpers/betinfoscreen_helper.rb
@@ -0,0 +1,2 @@
+module BetinfoscreenHelper
+end
